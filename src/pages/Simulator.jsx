47f176de1d4fa932f422f7816f76b1a9ffda9abf
Sergy Ferreira <sergyferreira153@gmail.com>
2026-07-09 20:44:59 -0500

    Inicio de cambio de preguntas

--- src/pages/Simulator.jsx
+++ src/pages/Simulator.jsx
@@ -1,5 +1,14 @@
+import { useState } from "react";
+import casos from "../data/casos.json";
+
 function Simulator() {
+
+  const [casoSeleccionado, setCasoSeleccionado] = useState(0);
+
+  const caso = casos[casoSeleccionado];
+
   return (
+  
     <div>
 
       <h1 className="text-4xl font-bold mb-3">
@@ -9,16 +18,40 @@ function Simulator() {
       <p className="text-gray-600 mb-10">
         Practica diagnósticos de soporte técnico nivel 1.
       </p>
+      <div className="flex flex-wrap gap-3 mb-8">
+
+  <button
+    onClick={() => setCasoSeleccionado(0)}
+    className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-lg"
+  >
+    No tengo internet
+  </button>
+
+  <button
+    onClick={() => setCasoSeleccionado(1)}
+    className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-lg"
+  >
+    WiFi lento
+  </button>
+
+  <button
+    onClick={() => setCasoSeleccionado(2)}
+    className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-lg"
+  >
+    Problemas DNS
+  </button>
+
+</div>
 
       <div className="bg-white rounded-xl shadow-lg p-8 max-w-3xl mx-auto">
 
-        <h2 className="text-xl font-semibold mb-4">
-          Caso: No tengo internet
-        </h2>
+       <h2 className="text-xl font-semibold mb-4">
+        Caso: {caso.titulo}
+      </h2>
 
-        <p className="text-lg mb-6">
-          ¿El router está encendido?
-        </p>
+      <p className="text-lg mb-6">
+        {caso.preguntaInicial}
+      </p>
 
        <div className="flex flex-col md:flex-row gap-4">
 
