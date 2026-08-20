4b2e9a7cd8fc8a9987bfb7dda9735311dc330261
Sergy Ferreira <sergyferreira153@gmail.com>
2026-08-20 08:07:14 -0500

    Mejorar diseño responsive en la Semana 17

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,8 @@
 function Sidebar({ setCurrentPage }) {
 
-  console.log("Sidebar recibió:", setCurrentPage);
 
   return (
-    <aside className="hidden md:block bg-gray-900 text-white w-64 min-h-screen p-6">
+    <aside className="hidden sm:block bg-gray-900 text-white w-64 min-h-screen p-6">
 
       <h2 className="text-lg font-semibold mb-8">
         Menú
